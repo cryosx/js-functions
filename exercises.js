0f104717f61db341caf666393fb5f73500f63638
cryosx <cyruswu.email@gmail.com>
2018-02-07 21:50:37 -1000

    finished ex10

--- exercises.js
+++ exercises.js
@@ -210,6 +210,14 @@ console.log(checkQuotient());
  * Console.log your result.
 */ 
 
+var bango3 = 1;
+var bango4 = 2;
+var bango5 = 3;
+
+function addThenSubtract(num1, num2, num3) {
+	return substract(add(num1, num2), num3);
+}
+console.log(addThenSubtract(bango3, bango4, bango5));
 
 
 /*
